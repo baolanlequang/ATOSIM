1d489891b440123a298aed84f876dd9b487bb88c
baolan2005 <baolan2005@gmail.com>
2026-05-30 09:50:53 +0200

    update analysis

---
+++ run_selfish.sh
@@ -0,0 +1,4 @@
+java -jar atosim.jar \
+     simulator/org.palladiosimulator.blockchainsystems.atomsim/optimized_deterministic_lhs_configurations.csv \
+     simulator/org.palladiosimulator.blockchainsystems.atomsim/testmodels \
+     simulator/org.palladiosimulator.blockchainsystems.atomsim/testmodels/configuration.json
