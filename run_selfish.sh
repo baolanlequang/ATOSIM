ca0559eda86f86384fa19c548b6ce6655f835c70
baolan2005 <baolan2005@gmail.com>
2026-05-31 09:35:44 +0200

    update model

--- run_selfish.sh
+++ run_selfish.sh
@@ -16,7 +16,7 @@ set -euo pipefail
 
 mkdir -p logs
 
-java -jar atosim.jar \
+java -Xms8g -Xmx56g -jar atosim.jar \
      simulator/org.palladiosimulator.blockchainsystems.atomsim/optimized_deterministic_lhs_configurations.csv \
      simulator/org.palladiosimulator.blockchainsystems.atomsim/testmodels \
      simulator/org.palladiosimulator.blockchainsystems.atomsim/testmodels/configuration.json
